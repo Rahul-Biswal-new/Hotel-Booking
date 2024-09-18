2ca3029ba3626a1431bdb84e99c5c2ee2983f5bb
RAHUL BISWAL <72703616+Rahul-Biswal-new@users.noreply.github.com>
2024-09-18 04:35:24 -0400

    fix

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Helmet } from 'react-helmet';
 import './Navbar.css'
 
 export default function Navbar() {
+  useEffect(() => {
+    document.title = "Eco Retreat";
+  }, []);
   return (
     <div className="navbar">
+      <Helmet>
+        <title>Eco Retreat</title>
+      </Helmet>
         <div className="navcontainer">
-            <span className="logo">Wanderlust Retreat</span>
+            <span className="logo">Eco Retreat</span>
             <div className="navitems">
                 <button className="navbutton">Register</button>
                 <button className="navbutton">Login</button>
